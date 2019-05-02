f8d69d521ed6169ccb8c38243f80e0f067099921
PDmatrix <dimasebakovgoo@gmail.com>
2019-05-02 22:25:49 +0300

    Removed offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,6 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
