10d755a78528e29a969a4f9d21677916842793ef
PDmatrix <dimasebakovgoo@gmail.com>
2019-10-28 00:07:48 +0300

    Add mdx support

---
+++ src/next/next.config.js
@@ -0,0 +1,23 @@
+const withPlugins = require('next-compose-plugins');
+const images = require('remark-images');
+const emoji = require('remark-emoji');
+const optimizedImages = require('next-optimized-images');
+
+const withMDX = require('@next/mdx')({
+	extension: /\.(md|mdx)$/,
+	options: {
+		remarkPlugins: [images, emoji]
+	}
+});
+
+module.exports = withPlugins([
+	[
+		withMDX,
+		{
+			pageExtensions: ['tsx', 'mdx']
+		}
+	],
+	[
+		optimizedImages
+	]
+]);
