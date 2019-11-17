479c970d2132a803fc065c592e17e16b81c92a83
PDmatrix <dimasebakovgoo@gmail.com>
2019-11-17 15:04:59 +0300

    Link in new tab

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,13 @@ const Footer = (): ReactElement => (
   <Row center={'xs'}>
     <Col xs={12} sm={11} md={10} lg={10}>
       <span>© 2019 | </span>
-      <a href="https://github.com/PDmatrix">Github</a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/PDmatrix"
+      >
+        Github
+      </a>
     </Col>
   </Row>
 );
